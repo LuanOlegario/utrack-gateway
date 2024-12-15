c917ad8636231fee46145d4ddee2be1aff9d1b17
Luan Olegario <luan.olegario@gg10.com.br>
2024-12-14 22:37:25 -0300

    Initial commit - utrack-gateway setup

---
+++ src/main/java/com/exemple/utrackbackend/UtrackGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.utrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtrackGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtrackGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exemple/utrackbackend/UtrackGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.utrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtrackGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
